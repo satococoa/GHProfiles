df83bb220a05a8ecd6db533a027d108ddfbf78b7
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-02-02 16:52:25 +0900

    Add github api proxy method

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,4 +2,12 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     true
   end
+
+  def github
+    Dispatch.once {
+      # TODO: あとでユーザ名とパスワードを入力されたものを使う
+      @github = UAGithubEngine.alloc.initWithUsername('username', password:'password', withReachability:true)
+    }
+    @github
+  end
 end
